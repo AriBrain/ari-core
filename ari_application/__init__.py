d0a7e6f619c6eacd3c3d570de8485c25aff9a7c4
lucp88 <lucaspeek@live.nl>
2025-05-13 12:15:22 +0200

    remove centralized imports

--- ari_application/__init__.py
+++ ari_application/__init__.py
@@ -1,16 +1,16 @@
 # Centralized imports
 
-from ui.upload_dialog import upload_file
-from .models.nifti_loader import NiftiLoader
-from .models.metrics import Metrics
+# from ui.upload_dialog import upload_file
+# from .models.nifti_loader import NiftiLoader
+# from .models.metrics import Metrics
 
-from .resources.styles import upload_button_styling, reset_button_styling
-
-
-from controllers.mouse_interactions import MouseInteractions
-
-from orth_views.orth_view_setup import OrthViewSetup
-from orth_views.orth_view_update import OrthViewUpdate
+# from .resources.styles import upload_button_styling, reset_button_styling
 
 
-from resources.styles import upload_button_styling, reset_button_styling
+# from controllers.mouse_interactions import MouseInteractions
+
+# from orth_views.orth_view_setup import OrthViewSetup
+# from orth_views.orth_view_update import OrthViewUpdate
+
+
+# from resources.styles import upload_button_styling, reset_button_styling
